6519239b3397fc34437cc515eecb62d0a74e2b7b
keisuke.kato <kgulliverk@hotmail.com>
2020-05-06 17:37:59 +0900

    課題質問

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ require 'active_support/time'
 module Clockwork
     handler do |job|
         sample = SlackNotify::Client.new(webhook_url: ENV['WEBHOOK'])
-        sample.notify("3分に1回送られるよ", "#rails_dev")
+        sample.notify("3分に1回送られるよ")
     end
     every(3.minutes, 'less.frequent.job')
 end
